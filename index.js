5336e34ce8cea881097bd48b8562d24792ad2226
Markus Oberlehner <markus.oberlehner@gmail.com>
2016-05-30 20:46:59 +0200

    rewrite all the things

--- index.js
+++ index.js
@@ -20,70 +20,137 @@ const defaultOptions = {
   extensions: ['.scss']
 };
 
-// Current working directory.
-let cwd;
-
 // Find selectors in the import url and
 // return a cleaned up url and the selectors.
 const parseUrl = (url) => {
   let cleanUrl = url;
   let selectorFilters;
-  let selectorFiltersMatch = url.match(/{([^}]+)}/);
+  const selectorFiltersMatch = url.match(/{([^}]+)}/);
+  let prioritizeModules = false;
   if (selectorFiltersMatch) {
     cleanUrl = url.split(' from ')[1].trim();
-    selectorFilters = selectorFiltersMatch[1].split(',').map(Function.prototype.call, String.prototype.trim);
+    // Create an array with selectors and replacement as one value.
+    selectorFilters = selectorFiltersMatch[1].split(',');
+    // Trim unnecessary whitespace.
+    selectorFilters = selectorFilters.map(Function.prototype.call, String.prototype.trim);
+    // Split selectors and replacement selectors into an array.
+    selectorFilters = selectorFilters.map((currentValue, index) => {
+      return currentValue.split(' as ').map(Function.prototype.call, String.prototype.trim);
+    });
   }
-  return { cleanUrl, selectorFilters };
+  if (cleanUrl.charAt(0) == '~') {
+    cleanUrl = cleanUrl.slice(1);
+    prioritizeModules = true;
+  }
+  return { cleanUrl, selectorFilters, prioritizeModules };
 };
 
 // Find the absolute path for a given url.
-const getAbsoluteUrl = (url) => {
+const getAbsoluteUrl = (url, includePath) => {
   let absoluteUrl = url;
   if (!path.isAbsolute(url)) {
-    absoluteUrl = path.join(cwd, url);
+    absoluteUrl = path.join(includePath, url);
   }
+  // Normalize a path, taking care of '..' and '.' parts.
+  absoluteUrl = path.normalize(absoluteUrl);
   return absoluteUrl;
 };
 
-// Normalizes an import url for storage.
-const normalizeUrl = (url) => {
-  url = getAbsoluteUrl(url);
-  // Normalize a path, taking care of '..' and '.' parts.
-  url = path.normalize(url);
-  const parsed = path.parse(url);
-  // Rremove the file extension.
-  url = url.slice(0, url.length - parsed.ext.length);
-  return url;
-};
-
-// Return possible import url variations.
-const getUrlVariants = (url) => {
-  const parsed = path.parse(url);
-  const urlVariants = [url];
-  if (!parsed.ext) {
-    // Add import url variations with all
-    // possible extensions and partial prefix.
+// Create possible variants of file paths
+// with enabled extensions and partial prefix.
+const getFilePathVariants = (filePath) => {
+  const filePathVariants = [];
+  const parsedFilePath = path.parse(filePath);
+  if (parsedFilePath.ext) {
+    filePathVariants.push(filePath);
+  } else {
     defaultOptions.extensions.forEach((extension) => {
-      urlVariants.push(`${parsed.dir}/${parsed.base}${extension}`);
-      urlVariants.push(`${parsed.dir}/_${parsed.base}${extension}`);
+      filePathVariants.push(`${path.join(parsedFilePath.dir, parsedFilePath.base)}${extension}`);
+      filePathVariants.push(`${path.join(parsedFilePath.dir, '_' + parsedFilePath.base)}${extension}`);
     });
   }
-  return urlVariants;
+  return filePathVariants;
+};
+
+const getFilePath = (url, includePaths) => {
+  let absoluteFilePath;
+  let filePathVariants;
+  let filePath = false;
+  includePaths.some((includePath) => {
+    absoluteFilePath = getAbsoluteUrl(url, includePath);
+    filePathVariants = getFilePathVariants(absoluteFilePath);
+    return filePathVariants.some((filePathVariant) => {
+      if (fs.existsSync(filePathVariant)) {
+        filePath = filePathVariant;
+        return true;
+      }
+    });
+  });
+  return filePath;
+};
+
+const getModuleFilePath = (url) => {
+// @TODO: Find main file
+// @TODO: Find style file
+  let searchPath = false;
+  let filePath = false;
+  // Search for the index file if only the module name is given.
+  if (!path.parse(url).dir) {
+    url = path.join(url, 'index');
+  }
+  const filePathVariants = getFilePathVariants(url);
+  filePathVariants.some((filePathVariant) => {
+    searchPath = findup(filePathVariant, { cwd: './node_modules' });
+    if (searchPath) {
+      filePath = searchPath;
+      return true;
+    }
+  });
+  return filePath;
+};
+
+const processSelectorFilters = (contents, selectorFilters) => {
+  return postcss(postcss.plugin('postcss-extract-selectors', (options) => {
+    return (css) => {
+      css.walkRules((rule) => {
+        let removeRule = false;
+        // Findout if the current rule has an whitelisted selector.
+        selectorFilters.some((selectorFilter) => {
+          removeRule = false
+          let searchSelector = selectorFilter[0];
+          let replacementSelector = selectorFilter[1];
+          let selectorArray = rule.selector.split(',');
+          let matchingSelectorIndex = selectorArray.indexOf(searchSelector);
+          // Check if there is a matching selector in the current rule.
+          if (matchingSelectorIndex > -1) {
+            if (replacementSelector) {
+              // Change the selector to match the given replacement selector.
+              selectorArray[matchingSelectorIndex] = replacementSelector;
+              rule.selector = selectorArray.join(',');
+            }
+            // Stop search and do not remove the rule.
+            return true;
+          }
+          // Current rule is not whitelisted, remove the rule.
+          removeRule = true;
+        });
+        // Remove the rule.
+        if (removeRule) {
+          rule.remove();
+        }
+      });
+    };
+  })).process(contents, { syntax: postcssScss }).css;
 };
 
 module.exports = function(url, prev, done) {
-  // Set the current working directory.
-  cwd = this.options.includePaths;
-  if (path.isAbsolute(prev)) {
-    cwd = path.dirname(prev);
-  }
-
+console.time('benchmark');
   // Add ".css" to the allowed extensions if CSS import is enabled.
   if (defaultOptions.cssImport && defaultOptions.extensions.indexOf('.css') === -1) {
     defaultOptions.extensions.push('.css');
   }
 
-  // Keep track of imported files per "session".
+  // Keep track of imported files.
   if (!importedMap.has(this.options.importer)) {
     importedMap.set(this.options.importer, new Set());
   }
@@ -91,117 +158,80 @@ module.exports = function(url, prev, done) {
 
   // Get a clean url (without selector filters) and
   // the selector filters from the import url.
-  let { cleanUrl, selectorFilters } = parseUrl(url);
+  let { cleanUrl, selectorFilters, prioritizeModules } = parseUrl(url);
 
-  // Import files if they have no "*" in the URL and add
-  // the path to the map of already imported paths.
-  if (!glob.hasMagic(cleanUrl)) {
-    // Find the path to the file and
-    // search for matching files inside the "node_modules" directory.
-    const urlVariants = getUrlVariants(cleanUrl);
-    let filePath;
-    urlVariants.forEach((variantUrl) => {
-      let searchPath = findup(variantUrl, { cwd: cwd });
-      if (searchPath) {
-        filePath = searchPath;
-        return;
-      }
-      // Look for matching files inside the node_modules directory.
-      searchPath = findup(variantUrl, { cwd: './node_modules', nocase: true });
-      if (searchPath) {
-        filePath = cleanUrl = searchPath;
-        return;
+  // Create an array of all paths to search for files.
+  let includePaths = [];
+  if (path.isAbsolute(prev)) {
+    includePaths.push(path.dirname(prev));
+  }
+  includePaths = includePaths.concat(this.options.includePaths.split(path.delimiter));
+
+  // Check if it is a glob url.
+  if (glob.hasMagic(cleanUrl)) {
+    const imports = [];
+    let selectorFiltersString = '';
+    if (selectorFilters) {
+      selectorFiltersString = `{ ${selectorFilters.map((filter) => filter.join(' as ')).join(',')} } from `;
+    }
+    includePaths.some((includePath) => {
+      let files = glob.sync(cleanUrl, {cwd: includePath});
+      files.forEach((file) => {
+        imports.push(`@import "${selectorFiltersString}${prioritizeModules ? '~' : ''}${path.join(includePath, file)}";`);
+      });
+      if (files.length) {
+        return true;
       }
     });
+    return {
+      contents: imports.join(this.options.linefeed)
+    };
+  }
 
-    // Extract selectors from the imported file and
-    // only import the given selectors.
-    if (filePath && (selectorFilters || (defaultOptions.cssImport && path.parse(filePath).ext == '.css'))) {
-      let contents = fs.readFileSync(filePath);
+  // Find the absolute file path.
+  let filePath;
+  if (prioritizeModules) {
+    filePath = getModuleFilePath(cleanUrl) || getFilePath(cleanUrl, includePaths);
+  } else {
+    filePath = getFilePath(cleanUrl, includePaths) || getModuleFilePath(cleanUrl);
+  }
 
-      if (selectorFilters) {
-        let whiteListedSelectors = selectorFilters;
-        let replacementSelectors = {};
+  // Check if the file is already imported.
+  if (defaultOptions.importOnce && (importedSet.has(filePath) || importedSet.has(cleanUrl))) {
+    return {
+      contents: "\n"
+    };
+  }
 
-        whiteListedSelectors.map((currentValue, index) => {
-          const selectorAndReplacement = currentValue.split(' as ');
-          if (selectorAndReplacement[1]) {
-            whiteListedSelectors[index] = selectorAndReplacement[0].trim();
-            replacementSelectors[index] = selectorAndReplacement[1].trim();
-          }
-        });
-
-        contents = postcss(postcss.plugin('postcss-extract-selectors', (options) => {
-          return (css) => {
-            css.walkRules((rule) => {
-              // Findout if the current rule has an whitelisted selector.
-              let whiteListSelectorIndex = false;
-              whiteListedSelectors.forEach((selector, index) => {
-                let selectorArray = rule.selector.split(',');
-                if (selectorArray.indexOf(selector) !== -1) {
-                  // The index of the whitelisted selector is saved so we can
-                  // replace the selector with a given replacement selector
-                  whiteListSelectorIndex = index;
-                  return;
-                }
-              });
-              if (whiteListSelectorIndex === false) {
-                // Remove the selector.
-                rule.remove();
-              } else if (replacementSelectors[whiteListSelectorIndex]) {
-                // Change the selector to match the given replacement selector.
-                rule.selector = replacementSelectors[whiteListSelectorIndex];
-              }
-            });
-          };
-        })).process(contents, { syntax: postcssScss }).css;
-      }
-
-      return {
-        contents: contents.toString()
-      };
-    }
-
-    let normalizedUrl = normalizeUrl(cleanUrl);
-    // The file is already imported.
-    if (defaultOptions.importOnce && importedSet.has(normalizedUrl)) {
-      return {
-        contents: "\n"
-      };
-    }
-    importedSet.add(normalizedUrl);
-
+  // If the importer can not find the file,
+  // we return the url and wish node-sass more luck.
+  if (!filePath) {
+    // Add the url to the imported urls.
+    importedSet.add(cleanUrl);
     return {
       file: cleanUrl
     };
   }
 
-  glob(cleanUrl, {cwd: cwd}, (err, files) => {
-    if (err) {
-      return console.error(err);
+  // Use the file path as url.
+  cleanUrl = filePath;
+
+  // Add the url to the imported urls.
+  importedSet.add(cleanUrl);
+
+  // Load the file contents if the file has a .css ending
+  // or selectors were found.
+  if (selectorFilters || (defaultOptions.cssImport && path.parse(cleanUrl).ext == '.css')) {
+    let contents = fs.readFileSync(cleanUrl).toString();
+    // Filter and (optionally) replace selectors.
+    if (selectorFilters) {
+      contents = processSelectorFilters(contents, selectorFilters);
     }
-
-    const imports = [];
-    for (const file of files) {
-      const parsedFile = path.parse(file);
-      // Skip files which do not match the given extensions.
-      if (defaultOptions.extensions.indexOf(parsedFile.ext) === -1) {
-        continue;
-      }
-
-      let normalizedUrl = normalizeUrl(file);
-      let selectorFiltersString = '';
-      const escaped = normalizedUrl.replace(/\\/g, "\\\\");
-      if (selectorFilters) {
-        selectorFiltersString = `{ ${selectorFilters.join(',')} } from `;
-      }
-      imports.push(`@import "${selectorFiltersString}${escaped}";${this.options.linefeed}`);
-    }
-
-    // Add the @import statements with files found via the
-    // glob pattern, to the contents so they are imported as usual.
-    done({
-      contents: imports.join("\n")
-    });
-  });
+    return {
+      contents: contents
+    };
+  }
+  return {
+    file: cleanUrl
+  };
 };
