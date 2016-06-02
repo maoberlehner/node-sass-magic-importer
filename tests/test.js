b46d45ccf65eeda9ee04701c8f01e8260c281321
Markus Oberlehner <markus.oberlehner@gmail.com>
2016-06-02 10:31:45 +0200

    add a test .js script to test the node-sass render function and to test custom magic importer options

---
+++ tests/test.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const sass = require('node-sass');
+const magicImporter = require('../index.js');
+
+const outFile = './css/dist/combined.css';
+
+sass.render({
+  file: './css/combined.scss',
+  importer: magicImporter,
+  outFile: outFile,
+  magicImporter: {
+    importOnce: true,
+    cssImport: true,
+    extensions: ['.scss']
+  }
+}, function(error, result) {
+  if (!error) {
+    // No errors during the compilation, write this result on the disk.
+    fs.writeFile(outFile, result.css);
+  }
+});
