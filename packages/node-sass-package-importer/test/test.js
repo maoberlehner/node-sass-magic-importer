c46ec0fa3169c5f9e29ec9fecf02fd0f6585c7db
Markus Oberlehner <markus.oberlehner@gmail.com>
2016-08-02 20:09:58 +0200

    cleanup and add basic tests and test files

--- packages/node-sass-package-importer/test/test.js
+++ packages/node-sass-package-importer/test/test.js
@@ -5,13 +5,13 @@ const fs = require('fs');
 const sass = require('node-sass');
 const path = require('path');
 
-const GlobImporter = require('../').GlobImporter;
+const PackageImporter = require('../').PackageImporter;
 
 chai.use(chaiAsPromised);
 
-describe('GlobImporter', () => {
+describe('PackageImporter', () => {
   it('should be a function', () => {
-    return expect(GlobImporter).to.be.a('function');
+    return expect(PackageImporter).to.be.a('function');
   });
 
   /**
@@ -19,77 +19,83 @@ describe('GlobImporter', () => {
    */
   describe('resolve()', () => {
     it('should return null', (done) => {
-      const url = 'path/without/glob/pattern.js';
+      const url = 'path/that/does/not/exist.scss';
       const expectedResult = null;
-      return expect(GlobImporter.resolve(url)).to.eventually.deep.equal(expectedResult).notify(done);
+      return expect(PackageImporter.resolve(url)).to.eventually.deep.equal(expectedResult).notify(done);
     });
 
-    it('should return object with contents property with @import statements', (done) => {
-      const url = 'files/test-resolve/**/*.scss';
-      const includePath = path.join(process.cwd(), 'test');
-      const expectedResult = { contents: `@import '${path.join(includePath, 'files/test-resolve/style1.scss')}';
-@import '${path.join(includePath, 'files/test-resolve/style2.scss')}';` };
-      return expect(GlobImporter.resolve(url, [includePath])).to.eventually.deep.equal(expectedResult).notify(done);
+    it('should return object with @import statement for the test-module main sass file', (done) => {
+      const url = 'test-module';
+      const includePath = path.join(process.cwd(), 'test/files/test-resolve');
+      const expectedResult = { contents: `@import '${path.join(includePath, 'node_modules/test-module/scss/style.scss')}';` };
+      return expect(PackageImporter.resolve(url, [includePath])).to.eventually.deep.equal(expectedResult).notify(done);
+    });
+
+    it('should return object with @import statement for the test-module partial file', (done) => {
+      const url = 'test-module/scss/partial';
+      const includePath = path.join(process.cwd(), 'test/files/test-resolve');
+      const expectedResult = { contents: `@import '${path.join(includePath, 'node_modules/test-module/scss/_partial.scss')}';` };
+      return expect(PackageImporter.resolve(url, [includePath])).to.eventually.deep.equal(expectedResult).notify(done);
     });
   });
 
   /**
    * importer()
    */
-   describe('importer()', () => {
-     const globImporter = new GlobImporter();
-
-     it('should resolve glob import', (done) => {
-       const testName = 'globbing';
-       const expectedResult = fs.readFileSync('test/files/glob-reference.css', { 'encoding': 'utf8' });
-       sass.render({
-         file: 'test/files/glob.scss',
-         importer: globImporter.importer()
-       }, (error, result) => {
-         if (!error) {
-           expect(result.css.toString()).to.equal(expectedResult);
-           done();
-         } else {
-           console.log(error);
-         }
-       });
-     });
-
-     it('should resolve glob import with include path', (done) => {
-       const testName = 'globbing';
-       const expectedResult = fs.readFileSync('test/files/include-path-reference.css', { 'encoding': 'utf8' });
-       sass.render({
-         file: 'test/files/include-path.scss',
-         importer: globImporter.importer(),
-         includePaths: [
-           'some/other/include-path',
-           'test/files/test-imports'
-         ],
-       }, (error, result) => {
-         if (!error) {
-           expect(result.css.toString()).to.equal(expectedResult);
-           done();
-         } else {
-           console.log(error);
-         }
-       });
-     });
-
-     it('should resolve glob import with absolute include path', (done) => {
-       const testName = 'globbing';
-       const expectedResult = fs.readFileSync('test/files/include-path-reference.css', { 'encoding': 'utf8' });
-       sass.render({
-         file: 'test/files/include-path.scss',
-         importer: globImporter.importer(),
-         includePaths: [process.cwd() + '/test/files/test-imports'],
-       }, (error, result) => {
-         if (!error) {
-           expect(result.css.toString()).to.equal(expectedResult);
-           done();
-         } else {
-           console.log(error);
-         }
-       });
-     });
-   });
+  //  describe('importer()', () => {
+  //    const globImporter = new GlobImporter();
+   // 
+  //    it('should resolve glob import', (done) => {
+  //      const testName = 'globbing';
+  //      const expectedResult = fs.readFileSync('test/files/glob-reference.css', { 'encoding': 'utf8' });
+  //      sass.render({
+  //        file: 'test/files/glob.scss',
+  //        importer: globImporter.importer()
+  //      }, (error, result) => {
+  //        if (!error) {
+  //          expect(result.css.toString()).to.equal(expectedResult);
+  //          done();
+  //        } else {
+  //          console.log(error);
+  //        }
+  //      });
+  //    });
+   // 
+  //    it('should resolve glob import with include path', (done) => {
+  //      const testName = 'globbing';
+  //      const expectedResult = fs.readFileSync('test/files/include-path-reference.css', { 'encoding': 'utf8' });
+  //      sass.render({
+  //        file: 'test/files/include-path.scss',
+  //        importer: globImporter.importer(),
+  //        includePaths: [
+  //          'some/other/include-path',
+  //          'test/files/test-imports'
+  //        ],
+  //      }, (error, result) => {
+  //        if (!error) {
+  //          expect(result.css.toString()).to.equal(expectedResult);
+  //          done();
+  //        } else {
+  //          console.log(error);
+  //        }
+  //      });
+  //    });
+   // 
+  //    it('should resolve glob import with absolute include path', (done) => {
+  //      const testName = 'globbing';
+  //      const expectedResult = fs.readFileSync('test/files/include-path-reference.css', { 'encoding': 'utf8' });
+  //      sass.render({
+  //        file: 'test/files/include-path.scss',
+  //        importer: globImporter.importer(),
+  //        includePaths: [process.cwd() + '/test/files/test-imports'],
+  //      }, (error, result) => {
+  //        if (!error) {
+  //          expect(result.css.toString()).to.equal(expectedResult);
+  //          done();
+  //        } else {
+  //          console.log(error);
+  //        }
+  //      });
+  //    });
+  //  });
 });
