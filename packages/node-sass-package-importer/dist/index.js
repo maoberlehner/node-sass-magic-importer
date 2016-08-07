5d06445fc98a992de721b960dd41c441fe3cee89
Markus Oberlehner <markus.oberlehner@gmail.com>
2016-08-07 18:46:13 +0200

    fix docblock documentation

--- packages/node-sass-package-importer/dist/index.js
+++ packages/node-sass-package-importer/dist/index.js
@@ -60,7 +60,7 @@ PackageImporter.prototype.resolveSync = function resolveSync (url) {
 /**
  * Asynchronously resolve the path to a node-sass import url.
  * @param {string} url - Import url from node-sass.
- * @return {Promise} Promise for node-sass importer return object.
+ * @return {Promise} Promise for a fully resolved import url.
  */
 PackageImporter.prototype.resolve = function resolve$1 (url) {
     var this$1 = this;
