8958790aa30d93c88013f79aabc4710544417199
Markus Oberlehner <markus.oberlehner@gmail.com>
2016-08-30 07:33:22 +0200

    start to cleanup the code

--- packages/node-sass-selector-importer/js/lib/SelectorImporter.js
+++ packages/node-sass-selector-importer/js/lib/SelectorImporter.js
@@ -1,28 +1,13 @@
 import path from 'path';
 
-export default class PackageImporter {
+export default class SelectorImporter {
   /**
    * Import packages from the `node_modules` directory.
    * @param {Object} options - Configuration options.
    */
   constructor(options = {}) {
     const defaultOptions = {
-      cwd: process.cwd(),
-      extensions: [
-        '.scss',
-        '.sass'
-      ],
-      packageKeys: [
-        'sass',
-        'scss',
-        'style',
-        'css',
-        'main.sass',
-        'main.scss',
-        'main.style',
-        'main.css',
-        'main'
-      ]
+      cwd: process.cwd()
     };
     this.options = Object.assign(defaultOptions, options);
   }
@@ -62,48 +47,5 @@ export default class PackageImporter {
     return new Promise((promiseResolve) => {
       promiseResolve(this.resolveSync(url));
     });
-  }
-
-  /**
-   * Clean a node sass import url.
-   * @param {string} url - Import url from node-sass.
-   * @return {string} Cleaned url.
-   */
-  cleanUrl(url) {
-    // Remove tilde symbol from the beginning
-    // of urls (except home "~/" directory).
-    const re = new RegExp(`^~(?!${path.sep})`);
-    return url.replace(re, '');
-  }
-
-  /**
-   * Create url variants for partial file matching (e.g. _file.scss).
-   * @param {string} url - Import url from node-sass.
-   * @return {Array} Multiple variants of sass file names.
-   */
-  urlVariants(url) {
-    const parsedUrl = path.parse(url);
-    let urlVariants = [url];
-    if (parsedUrl.dir && !parsedUrl.ext) {
-      urlVariants = this.options.extensions.reduce((x, extension) => {
-        x.push(path.join(parsedUrl.dir, `${parsedUrl.name}${extension}`));
-        x.push(path.join(parsedUrl.dir, `_${parsedUrl.name}${extension}`));
-        return x;
-      }, urlVariants);
-    }
-    return urlVariants;
-  }
-
-  /**
-   * Find the first matching key in a package.json file
-   * and set it as value for the `main` field.
-   * @param  {Object} pkg - Contents of a package.json.
-   * @return {Object} A package.json object with a replaced main value.
-   */
-  resolveFilter(pkg) {
-    const newPkg = pkg;
-    const pkgKey = this.options.packageKeys.find(x => pkg[x] !== undefined);
-    newPkg.main = pkg[pkgKey];
-    return newPkg;
   }
 }
