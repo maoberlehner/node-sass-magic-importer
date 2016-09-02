e8e1866cc82814e8cac5e8bd55e130662900f44f
Markus Oberlehner <markus.oberlehner@gmail.com>
2016-09-02 18:44:05 +0200

    fix object assign statements, fixes #5

--- packages/node-sass-package-importer/js/index.js
+++ packages/node-sass-package-importer/js/index.js
@@ -3,7 +3,7 @@ import PackageImporter from './lib/PackageImporter.js';
 const packageImporter = new PackageImporter();
 export default function (url, prev, done) {
   if (this.options.packageImporter) {
-    packageImporter.options = Object.assign(packageImporter.options, this.options.packageImporter);
+    Object.assign(packageImporter.options, this.options.packageImporter);
   }
   packageImporter.resolve(url).then(file => done(file ? { file } : null));
 }
