b1689f8ba8b79f6776806eedd7e8c25bfcf27d10
Markus Oberlehner <markus.oberlehner@gmail.com>
2017-01-06 16:17:18 +0100

    Add whitespace.

--- packages/node-sass-filter-importer/js/lib/parse-import-url.js
+++ packages/node-sass-filter-importer/js/lib/parse-import-url.js
@@ -4,6 +4,7 @@ import cleanImportUrl from './clean-import-url';
 export default (url) => {
   const cleanUrl = cleanImportUrl(url);
   const filters = extractImportFilters(url);
+
   return {
     url: cleanUrl,
     filters,
