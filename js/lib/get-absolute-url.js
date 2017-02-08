8fb34bfa9e79cf4e006d6d1222b58597196b3185
Markus Oberlehner <markus.oberlehner@gmail.com>
2017-02-08 20:22:44 +0100

    Resolve include path with dir and glob pattern.

--- js/lib/get-absolute-url.js
+++ js/lib/get-absolute-url.js
@@ -1,6 +1,10 @@
-export function getAbsoluteUrl({ path, getSassFileGlobPattern }, url) {
-  const { base } = path.parse(url);
-  getSassFileGlobPattern(base);
+export function getAbsoluteUrl({ path, getSassFileGlobPattern }, url, includePaths = []) {
+  const { dir, base } = path.parse(url);
+  const baseGlobPattern = getSassFileGlobPattern(base);
+  includePaths.some((includePath) => {
+    path.resolve(includePath, dir, baseGlobPattern);
+    return false;
+  });
 }
 
 export default function getAbsoluteUrlFactory() {
--- test/lib/get-absolute-url.test.js
+++ test/lib/get-absolute-url.test.js
@@ -8,7 +8,10 @@ test(`Should be a function.`, (t) => {
 });
 
 test(`Should call path.parse() with url.`, (t) => {
-  const path = { parse: sinon.stub().returns({}) };
+  const path = {
+    parse: sinon.stub().returns({}),
+    resolve() {},
+  };
   const getSassFileGlobPattern = () => {};
   const url = `some/url`;
 
@@ -18,9 +21,26 @@ test(`Should call path.parse() with url.`, (t) => {
 
 test(`Should call getSassFileGlobPattern() with the parsed file base.`, (t) => {
   const base = `test-file`;
-  const path = { parse: sinon.stub().returns({ base }) };
+  const path = {
+    parse: sinon.stub().returns({ base }),
+    resolve() {},
+  };
   const getSassFileGlobPattern = sinon.spy();
 
   getAbsoluteUrl({ path, getSassFileGlobPattern });
   t.true(getSassFileGlobPattern.calledWith(base));
+});
+
+test(`Should call path.resolve() with include path dir and a glob pattern base.`, (t) => {
+  const dir = `test/dir`;
+  const base = `test-file`;
+  const includePath = `include/path`;
+  const path = {
+    parse: sinon.stub().returns({ dir, base }),
+    resolve: sinon.spy(),
+  };
+  const getSassFileGlobPattern = sinon.stub().returns(`glob-pattern`);
+
+  getAbsoluteUrl({ path, getSassFileGlobPattern }, undefined, [includePath]);
+  t.true(path.resolve.calledWith(includePath, dir, `glob-pattern`));
 });
