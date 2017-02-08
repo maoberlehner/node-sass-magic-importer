bd25424d16d36929f854b3bd0773321a9ec5a5ed
Markus Oberlehner <markus.oberlehner@gmail.com>
2017-02-08 19:46:23 +0100

    Get sass file glob pattern from parsed base from url.

--- js/lib/get-absolute-url.js
+++ js/lib/get-absolute-url.js
@@ -1,5 +1,6 @@
-export function getAbsoluteUrl({ path }, url) {
-  path.parse(url);
+export function getAbsoluteUrl({ path, getSassFileGlobPattern }, url) {
+  const { base } = path.parse(url);
+  getSassFileGlobPattern(base);
 }
 
 export default function getAbsoluteUrlFactory() {
