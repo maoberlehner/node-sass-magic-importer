71a6dc9df76046f9c2ce7312e158b281c117bc4a
Markus Oberlehner <markus.oberlehner@gmail.com>
2017-02-08 19:46:23 +0100

    Get sass file glob pattern from parsed base from url.

--- test/lib/get-absolute-url.test.js
+++ test/lib/get-absolute-url.test.js
@@ -8,9 +8,19 @@ test(`Should be a function.`, (t) => {
 });
 
 test(`Should call path.parse() with url.`, (t) => {
-  const path = { parse: sinon.spy() };
+  const path = { parse: sinon.stub().returns({}) };
+  const getSassFileGlobPattern = () => {};
   const url = `some/url`;
 
-  getAbsoluteUrl({ path }, url);
+  getAbsoluteUrl({ path, getSassFileGlobPattern }, url);
   t.true(path.parse.calledWith(url));
+});
+
+test(`Should call getSassFileGlobPattern() with the parsed file base.`, (t) => {
+  const base = `test-file`;
+  const path = { parse: sinon.stub().returns({ base }) };
+  const getSassFileGlobPattern = sinon.spy();
+
+  getAbsoluteUrl({ path, getSassFileGlobPattern });
+  t.true(getSassFileGlobPattern.calledWith(base));
 });
