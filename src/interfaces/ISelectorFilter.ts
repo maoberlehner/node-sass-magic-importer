7e7d404bca97f7bb91b6ea13fa8f0fb131bbf4ea
Markus Oberlehner <markus.oberlehner@gmail.com>
2017-04-17 16:14:37 +0200

    Use extend for describing the raw selector filter interface

--- src/interfaces/ISelectorFilter.ts
+++ src/interfaces/ISelectorFilter.ts
@@ -3,7 +3,6 @@ export interface ISelectorFilter {
   replacement: any;
 }
 
-export interface ISelectorFilterRaw {
+export interface ISelectorFilterRaw extends ISelectorFilter {
   selector: string;
-  replacement: any;
 }
