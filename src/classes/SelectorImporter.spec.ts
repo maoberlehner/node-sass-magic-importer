afbabbce9d06c6a76a39a52e83f36508b16f37fc
Markus Oberlehner <markus.oberlehner@gmail.com>
2017-04-17 16:30:08 +0200

    Add SelectorImporter class test case

--- src/classes/SelectorImporter.spec.ts
+++ src/classes/SelectorImporter.spec.ts
@@ -28,3 +28,32 @@ test(`Should be an object.`, (t) => {
 
   t.is(typeof selectorImporter, `object`);
 });
+
+test(`Should parse selector filters, clean and resolve the given URL and extract selectors.`, (t) => {
+  const url = `test/url`;
+  const includePaths = [`/`];
+
+  const cleanImportUrlStub = sinon.stub().returns(`cleaned/url`);
+  const cssSelectorExtractStub = { processSync: sinon.stub() };
+  const fsStub = { readFileSync: sinon.stub().returns(`css`) };
+  const parseSelectorFiltersStub = sinon.stub().returns([`selectorFilters`]);
+  const resolveUrlStub = sinon.stub().returns(`resolved/url`);
+  const dependencies = Object.assign(
+    t.context.dependencies,
+    {
+      cleanImportUrl: cleanImportUrlStub,
+      cssSelectorExtract: cssSelectorExtractStub,
+      fs: fsStub,
+      parseSelectorFilters: parseSelectorFiltersStub,
+      resolveUrl: resolveUrlStub,
+    },
+  );
+  const selectorImporter = new SelectorImporter(dependencies);
+
+  selectorImporter.import(url, includePaths);
+
+  t.true(parseSelectorFiltersStub.calledWith(url));
+  t.true(cleanImportUrlStub.calledWith(url));
+  t.true(resolveUrlStub.calledWith(`cleaned/url`, includePaths));
+  t.true(cssSelectorExtractStub.processSync.calledWith(`css`, [`selectorFilters`]));
+});
