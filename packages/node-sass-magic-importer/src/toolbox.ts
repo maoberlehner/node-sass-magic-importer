3b682e6e84c825d382b7a41813709e8bb4ef26b4
Markus Oberlehner <markus.oberlehner@gmail.com>
2017-07-12 18:09:42 +0200

    Add function to resolve glob urls

--- packages/node-sass-magic-importer/src/toolbox.ts
+++ packages/node-sass-magic-importer/src/toolbox.ts
@@ -14,6 +14,7 @@ import { extractSelectorsFactory } from './functions/extract-selectors';
 import { parseNodeFiltersFactory } from './functions/parse-node-filters';
 import { parseSelectorFiltersFactory } from './functions/parse-selector-filters';
 import { processRawSelectorFiltersFactory } from './functions/process-raw-selector-filters';
+import { resolveGlobUrlFactory } from './functions/resolve-glob-url';
 import { resolvePackageKeyFactory } from './functions/resolve-package-key';
 import { resolvePackageUrlFactory } from './functions/resolve-package-url';
 import { resolveUrlFactory } from './functions/resolve-url';
@@ -21,6 +22,7 @@ import { sassGlobPatternFactory } from './functions/sass-glob-pattern';
 import { sassUrlVariantsFactory } from './functions/sass-url-variants';
 import { splitSelectorFilterFactory } from './functions/split-selector-filter';
 
+// Import interfaces to help TypeScript build the definitions.
 import { ISelectorFilter, ISelectorFilterRaw } from './interfaces/ISelectorFilter';
 
 export const buildIncludePaths = buildIncludePathsFactory(path);
@@ -33,6 +35,7 @@ export const processRawSelectorFilters = processRawSelectorFiltersFactory(escape
 export const resolvePackageKey = resolvePackageKeyFactory();
 export const sassGlobPattern = sassGlobPatternFactory(path);
 export const sassUrlVariants = sassUrlVariantsFactory(path);
+export const resolveGlobUrl = resolveGlobUrlFactory(glob, path);
 export const resolvePackageUrl = resolvePackageUrlFactory(resolve, resolvePackageKey, sassUrlVariants);
 export const resolveUrl = resolveUrlFactory(glob, path, sassGlobPattern);
 export const splitSelectorFilter = splitSelectorFilterFactory();
