f822800b630426564da2773cfc2c5cd894f5e22c
Kon Pik <kopik@microsoft.com>
2018-03-20 21:07:16 -0700

    typo, add both caches

--- packages/node-sass-once-importer/src/index.ts
+++ packages/node-sass-once-importer/src/index.ts
@@ -18,6 +18,8 @@ export = function onceImporter() {
 
   // cache for path resolution
   let includePathsCache: Map<string, string[]> = new Map();
+  // cache for resolvedUrls
+  let resolvedUrlsCache: Map<string, string> = new Map();
 
   return function importer(url: string, prev: string) {
     const nodeSassOptions = this.options;
@@ -33,17 +35,21 @@ export = function onceImporter() {
     // would be detected as multiple imports of the same file.
     const store = this.nodeSassOnceImporterContext.store;
     // per instance of new importer created, path resolution is shared
-    if (!includePathsCache.has(url)) {
+    if (!includePathsCache.has(nodeSassOptions.includePaths)) {
       includePathsCache.set(url, buildIncludePaths(
         nodeSassOptions.includePaths,
         prev,
       ));
     }
-    const includePaths = includePathsCache.get(url);
-    const resolvedUrl = resolveUrl(
-      url,
-      includePaths,
-    );
+    const includePaths = includePathsCache.get(nodeSassOptions.includePaths);
+
+    if (!resolvedUrlsCache.has(url)){
+      resolvedUrlsCache.set(url, resolveUrl(
+        url,
+        includePaths,
+      ));
+    }
+    const resolvedUrl = resolvedUrlsCache.get(url);
 
     if (store.has(resolvedUrl)) {
       return EMPTY_IMPORT;
