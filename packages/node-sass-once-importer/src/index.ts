2be2aa2d1b1c3f4bdcc68baf86bfbb3b01bb1fd7
Mansoor <mansoor.omrani@gmail.com>
2020-08-31 10:22:32 +0430

    Used md5 to import scss/css files exactly one time based on their content

--- packages/node-sass-once-importer/src/index.ts
+++ packages/node-sass-once-importer/src/index.ts
@@ -2,20 +2,27 @@ import {
   buildIncludePaths,
   resolveUrl,
 } from 'node-sass-magic-importer/dist/toolbox';
+import { ImporterReturnType, AsyncImporter } from 'node-sass';
+import * as md5 from 'nodejs-md5';
 
 const EMPTY_IMPORT = {
   file: ``,
   contents: ``,
 };
 
-export = function onceImporter() {
-  return function importer(url: string, prev: string) {
+interface NodeSassOnceImporterContext {
+  store: Set<string>
+}
+export = function onceImporter(): AsyncImporter {
+  let nodeSassOnceImporterContext: NodeSassOnceImporterContext;
+
+  return function importer(url: string, prev: string, done: (data: ImporterReturnType) => void) {
     const nodeSassOptions = this.options;
     // Create a context for the current importer run.
     // An importer run is different from an importer instance,
     // one importer instance can spawn infinite importer runs.
-    if (!this.nodeSassOnceImporterContext) {
-      this.nodeSassOnceImporterContext = {
+    if (!nodeSassOnceImporterContext) {
+      nodeSassOnceImporterContext = {
         store: new Set(),
       };
     }
@@ -23,22 +30,41 @@ export = function onceImporter() {
     // Each importer run has it's own new store, otherwise
     // files already imported in a previous importer run
     // would be detected as multiple imports of the same file.
-    const store = this.nodeSassOnceImporterContext.store;
-    const includePaths = buildIncludePaths(
-      nodeSassOptions.includePaths,
-      prev,
-    );
+    const store = nodeSassOnceImporterContext.store;
+
+    let includePaths: Array<string> = [];
+
+    if (nodeSassOptions.includePaths) {
+      for (let path of nodeSassOptions.includePaths) {
+        const paths = buildIncludePaths(path, prev);
+        includePaths = [...includePaths, ...paths];
+      }
+    } else {
+      includePaths = buildIncludePaths('', prev);
+    }
+
     const resolvedUrl = resolveUrl(
       url,
       includePaths,
     );
 
-    if (store.has(resolvedUrl)) {
-      return EMPTY_IMPORT;
+    if (!md5 || !md5.file || !md5.file.quiet) {
+      throw 'MD5 is not present!';
     }
 
-    store.add(resolvedUrl);
+    md5.file.quiet(resolvedUrl, function (err: any, result: string) {
+      if (err) {
+        // fallback to url
+        result = resolvedUrl;
+      }
 
-    return null;
+      if (store.has(result)) {
+        done(EMPTY_IMPORT);
+      } else {
+        store.add(resolvedUrl);
+
+        done({ file: resolvedUrl });
+      }
+    });
   };
 };
