0a9c1b31a7b05ab5dbd3e84d0b23a5cacf26b63d
Markus Oberlehner <markus.oberlehner@gmail.com>
2020-10-06 16:03:47 +0200

    Prevent upgrade to unsuported dependency versions

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -2,12 +2,12 @@
 
 rm -Rf package-lock.json
 
-node_modules/.bin/npm-check-updates -u
+node_modules/.bin/npm-check-updates -u --reject bootstrap
 
 npm install
 
 for package in packages/*; do
-  ( cd $package && ../../node_modules/.bin/npm-check-updates -u )
+  ( cd $package && ../../node_modules/.bin/npm-check-updates -u --reject css-node-extract,css-selector-extract )
 done
 
 npm run bootstrap
